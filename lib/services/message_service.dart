a0e633edce632434928542ef9011db528c2f5502
Esteban Ficetti <ficettiesteban@gmail.com>
2023-09-21 14:32:06 -0300

    changed status management for sending message

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,16 +1,32 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:twilio_dart/exceptions/message_failed_exception.dart';
 import 'package:twilio_dart/models/credential.dart';
 import 'package:twilio_dart/models/message.dart';
 import 'package:twilio_dart/models/messages_data.dart';
 import 'package:twilio_dart/services/network_service.dart';
 
+/// This is the enum that manage the status from TwilioAPI
+/// Only missing delivery_unknown due to the fact that it is not important.
+enum TwilioStatus {
+  queued,
+  accepted,
+  scheduled,
+  sending,
+  sent,
+  delivered,
+  undelivered,
+  failed,
+}
+
 class MessageService {
   static const int successCodeStart = 200;
   static const int successCodeEnd = 299;
 
-  static bool statusInSuccess(int status) {
-    return status >= successCodeStart && status <= successCodeEnd;
+  TwilioStatus _getStatusFromResponse(String status) {
+    return TwilioStatus.values.firstWhere(
+      (element) => element.name == status,
+    );
   }
 
   Future<MessagesData?> getMessageList({
@@ -29,9 +45,13 @@ class MessageService {
     if (response != null) {
       final responseMap = json.decode(response.body) as Map<String, dynamic>;
 
-      final status = int.tryParse(responseMap['status'].toString());
-      if (status != null && statusInSuccess(status)) {
-        throw MessageFailedException(status, responseMap['message'].toString());
+      final status = _getStatusFromResponse(responseMap['status'].toString());
+
+      if (status == TwilioStatus.failed || status == TwilioStatus.undelivered) {
+        throw MessageFailedException(
+          HttpStatus.badRequest,
+          responseMap['message'].toString(),
+        );
       }
 
       final messagesData = MessagesData.fromJSON(responseMap);
@@ -48,18 +68,25 @@ class MessageService {
     final body = <String, String>{
       'From': Credential.instance.twilioNumber,
       'To': toNumber,
-      'Body': message
+      'Body': message,
     };
+
     final response = await NetworkService.instance.post(url, body);
+
     if (response != null) {
       final responseMap = json.decode(response.body) as Map<String, dynamic>;
 
-      final status = int.tryParse(responseMap['status'].toString());
-      if (status != null && statusInSuccess(status)) {
-        throw MessageFailedException(status, responseMap['message'].toString());
+      final status = _getStatusFromResponse(responseMap['status'].toString());
+
+      if (status == TwilioStatus.failed || status == TwilioStatus.undelivered) {
+        throw MessageFailedException(
+          HttpStatus.badRequest,
+          responseMap['message'].toString(),
+        );
       }
 
       final message = Message.fromJSON(responseMap);
+
       return message;
     } else {
       throw Exception('Response is null');
